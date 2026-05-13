844b0151534cd3a8f550ba76cf08d9ce9e17ff37
adventurescumm <gab05mas@gmail.com>
2026-05-13 11:37:53 -0300

    Pequena otimização

--- models.py
+++ models.py
@@ -2,6 +2,7 @@ import uuid
 import enum
 from datetime import datetime
 from db import db
+from sqlalchemy import Identity
 
 class CourseStatus(enum.Enum):
     DISPONIVEL = "DISPONIVEL"
@@ -14,6 +15,6 @@ class Course(db.Model):
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), nullable=False, unique=True)
     name_instructor = db.Column(db.String(100), nullable=False)
-    code_course = db.Column(db.Integer, unique=True, nullable=False, autoincrement=True)
+    code_course = db.Column(db.Integer, Identity(start=1), unique=True, nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
     status = db.Column(db.Enum(CourseStatus, name="course_status"), nullable=False, default=CourseStatus.DISPONIVEL)
