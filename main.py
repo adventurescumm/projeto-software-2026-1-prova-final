8b6788f850f372c167cb69c9d8a11f9f88bb2e35
adventurescumm <gab05mas@gmail.com>
2026-05-13 10:54:44 -0300

    Arquivos implementados

---
+++ main.py
@@ -0,0 +1,80 @@
+from flask import Flask, request, jsonify
+from db import db
+from models import Course
+from validate import token_required
+import os
+
+def create_app():
+    app = Flask(__name__)
+    
+    postgres_user = os.environ.get('POSTGRES_USER', 'appuser')
+    postgres_password = os.environ.get('POSTGRES_PASSWORD', 'apppass')
+    postgres_url = os.environ.get('POSTGRES_URL', 'localhost')
+    
+    # Define o padrão, mas permite que o Pytest sobrescreva depois
+    db_uri = f"postgresql://{postgres_user}:{postgres_password}@{postgres_url}:5432/courses"
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DATABASE_URI", db_uri)
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    
+    db.init_app(app)
+
+
+    @app.route("/courses", methods=["POST"])
+    @token_required("ADMIN")
+    def create_course():
+        data = request.json
+
+        course = Course(
+            name=data["name"],
+            email=data["email"],
+            name_instructor=data["name_instructor"],
+        )
+
+        db.session.add(course)
+        db.session.commit()
+
+        return jsonify({
+            "id": str(course.id),
+            "name": course.name,
+            "email": course.email,
+            "name_instructor": course.name_instructor,
+            "code_course": course.code_course,
+            "created_at": course.created_at,
+            "status": course.status
+        }), 201
+
+    @app.route("/courses/<uuid:course_id>", methods=["DELETE"])
+    @token_required("ADMIN")
+    def delete_course(course_id):
+        course = Course.query.get_or_404(course_id)
+
+        db.session.delete(course)
+        db.session.commit()
+
+        return "", 204
+
+    @app.route("/courses", methods=["GET"])
+    @token_required(None)
+    def list_courses():
+        courses = Course.query.all()
+
+        return [
+            {
+            "id": str(course.id),
+            "name": course.name,
+            "email": course.email,
+            "name_instructor": course.name_instructor,
+            "code_course": course.code_course,
+            "created_at": course.created_at,
+            "status": course.status
+            }
+            for course in courses
+        ], 200
+
+    return app
+
+app = create_app()
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
---
+++ models.py
@@ -0,0 +1,19 @@
+import uuid
+import enum
+from datetime import datetime
+from db import db
+
+class CourseStatus(enum.Enum):
+    DISPONIVEL = "DISPONIVEL"
+    CANCELADO = "CANCELADO"
+
+class Course(db.Model):
+    __tablename__ = "courses"
+
+    id = db.Column(db.UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    name_instructor = db.Column(db.String(100), nullable=False)
+    code_course = db.Column(db.Integer, unique=True, nullable=False, autoincrement=True)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    status = db.Column(db.Enum(CourseStatus, name="course_status"), nullable=False, default=CourseStatus.DISPONIVEL)
